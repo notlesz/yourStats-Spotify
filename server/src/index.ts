51032628767c963b96ef640d7f1cb80b37604126
Elton Souza <elton.souza1515@gmail.com>
2022-12-30 19:10:11 -0300

    fix: update port server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,14 @@
-import express from 'express'
-import { routes } from './routes';
 import cors from "cors";
+import express from "express";
+import { routes } from "./routes";
 
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 
+const PORT = process.env.PORT || 3333;
 
-app.listen(3333, () => {
-    console.log('HTTP server started on port 3333');
-})
+app.listen(PORT, () => {
+  console.log(`Server is running in PORT:${PORT}`);
+});
