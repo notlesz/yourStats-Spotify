bd07b9372e1ed53ba0be398883a0167601e0af37
Elton Souza <elton.souza1515@gmail.com>
2023-01-01 19:45:56 -0300

    fix: remove token and usage toast message

--- web/src/context/userContext.tsx
+++ web/src/context/userContext.tsx
@@ -1,5 +1,6 @@
-import React, { ReactNode, createContext, useState, useEffect } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useToast from "../hook/useToast";
 import { User } from "../types/user";
 import removeAllKeys from "../utils/removeKeys";
 
@@ -10,7 +11,7 @@ interface userProviderProps {
 interface userContext {
   user: User | null;
   logout: () => void;
-  signIn: (user: User) => void;
+  signIn: (user: User, token: string) => void;
 }
 
 export const UserContext = createContext<userContext>({} as any);
@@ -18,27 +19,34 @@ export const UserContext = createContext<userContext>({} as any);
 export function UserProvider({ children }: userProviderProps) {
   const [user, setUser] = useState<User | null>(null);
   const navigate = useNavigate();
+  const { handleToast } = useToast();
+  const recoveredUser = localStorage.getItem("user");
 
   function logout() {
     setTimeout(() => {
       setUser(null);
       removeAllKeys();
       navigate("/");
-    }, 3000)
+      handleToast("success", "Logout realizado com sucesso!");
+    }, 3000);
   }
 
-  function signIn(user: User) {
+  function signIn(user: User, token: string) {
     setUser(user);
     localStorage.setItem("user", JSON.stringify(user));
-    navigate('/home')
+    localStorage.setItem("token_user", token);
+    navigate("/home");
   }
 
   useEffect(() => {
-    const recoveredUser = localStorage.getItem("user");
     if (recoveredUser) {
       setUser(JSON.parse(recoveredUser));
     }
   }, []);
 
-  return <UserContext.Provider value={{user, signIn, logout}}>{children}</UserContext.Provider>;
+  return (
+    <UserContext.Provider value={{ user, signIn, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
