e8dc429483e2a8af2c068978a082cc0370389406
Elton Souza <elton.souza1515@gmail.com>
2023-01-01 19:44:37 -0300

    feat: update login page

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -11,16 +11,20 @@ export default function Login() {
   };
 
   return (
-    <div className="h-screen w-full flex justify-center items-center bg-gray-900">
-      <h2>Your Stats</h2>
-      <p>View</p>
+    <div className="h-screen w-full flex flex-col gap-20 justify-center items-center bg-black text-white">
+      <div>
+        <p className="text-4xl font-normal font-russoOne text-green-600 text-center mb-4">
+          Hello There!
+        </p>
+        <h2 className="text-4xl font-bold">See your stats on Spotify</h2>
+      </div>
       <button
         type="button"
-        className="text-gray-900 flex items-center gap-2 bg-green-600 font-bold rounded-lg px-4 py-3 cursor-pointer hover:bg-green-700"
+        className="font-bold flex items-center gap-2 bg-green-600 rounded-lg px-4 py-3 cursor-pointer hover:bg-green-700"
         onClick={redirectToLogin}
       >
         <FaSpotify size={20} />
-        Login with Spotify
+        Sign In
       </button>
     </div>
   );
