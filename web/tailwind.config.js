c26abea61c7359071511671590afe77e4e6cc3c7
Elton Souza <elton.souza1515@gmail.com>
2023-01-01 19:36:04 -0300

    feat: add new theme colors, animations, update bg color app

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,56 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: { max: "695px" },
+      },
+      colors: {
+        gold: {
+          500: "#FFD700",
+        },
+        silver: {
+          500: "#C0C0C0",
+        },
+        bronze: {
+          500: "#CD7F32",
+        },
+        gray: {
+          100: "#63707F",
+          300: "#3D4554",
+          600: "#14171C",
+        },
+      },
+      fontFamily: {
+        russoOne: ["Russo One", "sans-serif"],
+      },
+      animation: {
+        hideToShow: "hideToShow 1s ease-in-out",
+        leftToShow: "leftToShow 1s ease-in-out",
+      },
+      keyframes: {
+        hideToShow: {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+        leftToShow: {
+          "0%": {
+            opacity: 0,
+            marginRight: 400,
+            marginLeft: -400,
+          },
+          "100%": {
+            opacity: 1,
+            marginRight: 0,
+            marginLeft: 0
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
