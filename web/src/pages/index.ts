88405ae4be598f6d6abf084e32004656297e7596
Elton Souza <elton.souza1515@gmail.com>
2023-01-02 12:16:30 -0300

    fix: remove import

--- web/src/pages/index.ts
+++ web/src/pages/index.ts
@@ -1,5 +1,3 @@
 export { default as LoginCallback } from './Callback';
 export { default as Home } from './Home';
 export { default as Login } from './Login';
-export { default as PlaylistInfo } from './PlaylistInfo';
-
