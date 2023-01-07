286712f732bbc8f8ca1a6fb5932301777dcd983a
Elton Souza <elton.souza1515@gmail.com>
2023-01-07 15:16:16 -0300

    refact: add more components in PrivateRoute

--- web/src/Router.tsx
+++ web/src/Router.tsx
@@ -1,10 +1,12 @@
 import { ReactNode, useContext } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import Header from './components/Header';
 import ScrollToTop from './components/ScrollToTop';
 import { UserContext, UserProvider } from './context/userContext';
 import { Home, Login, LoginCallback } from './pages';
 import PlaylistAll from './pages/PlaylistAll';
+import TopContent from './pages/TopContent';
 
 interface PrivateRouteProps {
   children: ReactNode;
@@ -13,7 +15,16 @@ interface PrivateRouteProps {
 const PrivateRoute = ({ children }: PrivateRouteProps) => {
   const { user } = useContext(UserContext);
   const token = localStorage.getItem('token_user');
-  return !token && !user ? <Navigate to='/' /> : <>{children}</>;
+  return !token && !user ? (
+    <Navigate to='/' />
+  ) : (
+    <div className='w-full min-h-screen'>
+      <div className='max-w-[1350px] mx-auto my-0 flex flex-col justify-center gap-[30px]'>
+        <Header />
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export function Router() {
@@ -49,6 +60,14 @@ export function Router() {
           element={
             <PrivateRoute>
               <PlaylistAll />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/top/:type'
+          element={
+            <PrivateRoute>
+              <TopContent />
             </PrivateRoute>
           }
         />
