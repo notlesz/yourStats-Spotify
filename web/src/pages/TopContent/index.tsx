5568e80a6fdc6e56ed8c53f54d61d7819eac15ca
Elton Souza <elton.souza1515@gmail.com>
2023-01-07 15:18:45 -0300

    feat: top content page

---
+++ web/src/pages/TopContent/index.tsx
@@ -0,0 +1,158 @@
+import classNames from 'classnames';
+import { Fragment } from 'react';
+import { FaSpotify } from 'react-icons/fa';
+import { IoMdArrowRoundBack } from 'react-icons/io';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Filter } from '../../components/Filter';
+import Loading from '../../components/Loading';
+import useMediaQuery from '../../hooks/useMediaQuery';
+import useSpotify from '../../hooks/useSpotify';
+
+export default function TopContent() {
+  const { type } = useParams<{ type: 'tracks' | 'artists' }>();
+  const {
+    artists: allTopArtists,
+    tracks: allTopTracks,
+    handleTimeRange,
+    timeRangeTracks,
+    isFetchingTracks,
+    timeRangeArtists,
+    isFetchingArtists,
+  } = useSpotify();
+
+  const navigate = useNavigate();
+  const matches = useMediaQuery('(max-width:580px)');
+
+  return (
+    <main className='mb-8 animate-leftToShow p-4'>
+      <h4 className='text-white text-center text-3xl font-bold mb-8 sm:text-2xl font-russoOne capitalize'>
+        Top {type}
+      </h4>
+      <div className='flex items-center justify-between mb-8'>
+        <button
+          className='flex items-center gap-2 text-white text-lg font-bold hover:text-green-600'
+          onClick={() => navigate('/home')}
+        >
+          <IoMdArrowRoundBack />
+          Back
+        </button>
+        {type === 'artists' ? (
+          <Filter
+            setTimeRange={handleTimeRange}
+            typeContent={type!}
+            timeRangeArtists={timeRangeArtists}
+          />
+        ) : (
+          <Filter
+            setTimeRange={handleTimeRange}
+            typeContent={type!}
+            timeRangeTracks={timeRangeTracks}
+          />
+        )}
+      </div>
+      <section className='flex flex-col w-full rounded bg-gray-600 p-8 gap-8 md:p-4 md:gap-4'>
+        {type === 'artists' && (
+          <>
+            {isFetchingArtists ? (
+              <div className='w-full h-[600px] flex justify-center items-center m-auto'>
+                <Loading size='medium' />
+              </div>
+            ) : (
+              <>
+                {allTopArtists?.map((artists, index) => (
+                  <Fragment key={artists.id}>
+                    <div className='flex items-center justify-between gap-2'>
+                      <div className='flex items-center gap-6 md:gap-3'>
+                        <span
+                          className={classNames(
+                            'text-white font-semibold md:text-xs flex gap-1 w-7 h-7 items-center justify-center rounded-full ',
+                            {
+                              'bg-gold-500': index + 1 === 1,
+                              'bg-silver-500': index + 1 === 2,
+                              'bg-bronze-500': index + 1 === 3,
+                            },
+                          )}
+                        >
+                          {index + 1} {index + 1 > 3 && <strong>º</strong>}
+                        </span>
+                        <img
+                          src={artists.images[0].url}
+                          alt=''
+                          className='w-[80px] h-[80px] rounded-md object-cover md:w-[60px] md:h-[60px] xs:w-[40px] xs:h-[40px]'
+                        />
+                        <p className='text-white font-semibold md:text-sm max-w-sm xs:max-w-[90px] md:max-w-[200px] text-ellipsis whitespace-nowrap overflow-hidden '>
+                          {artists.name}
+                        </p>
+                      </div>
+                      <button
+                        type='button'
+                        className='font-bold flex items-center gap-2 text-base text-green-600 px-3 py-2 cursor-pointer hover:text-green-800 md:text-xs'
+                        onClick={() => {
+                          window.open(artists.external_urls.spotify, '__blank');
+                        }}
+                      >
+                        <FaSpotify className='w-5 h-5' />
+                        {!matches && 'Open in Spotify'}
+                      </button>
+                    </div>
+                    {index + 1 === allTopArtists.length ? null : <hr className='border-gray-450' />}
+                  </Fragment>
+                ))}
+              </>
+            )}
+          </>
+        )}
+        {type === 'tracks' && (
+          <>
+            {isFetchingTracks ? (
+              <div className='w-full h-[600px] flex justify-center items-center m-auto'>
+                <Loading size='medium' />
+              </div>
+            ) : (
+              <></>
+            )}
+            {allTopTracks?.map((track, index) => (
+              <Fragment key={track.id}>
+                <div className='flex items-center justify-between gap-2'>
+                  <div className='flex items-center gap-6 md:gap-3'>
+                    <span
+                      className={classNames(
+                        'text-white font-semibold md:text-xs flex gap-1 w-7 h-7 items-center justify-center rounded-full ',
+                        {
+                          'bg-gold-500': index + 1 === 1,
+                          'bg-silver-500': index + 1 === 2,
+                          'bg-bronze-500': index + 1 === 3,
+                        },
+                      )}
+                    >
+                      {index + 1} {index + 1 > 3 && <strong>º</strong>}
+                    </span>
+                    <img
+                      src={track.album.images[0].url}
+                      alt=''
+                      className='w-[80px] h-[80px] rounded-md object-cover md:w-[60px] md:h-[60px] xs:w-[40px] xs:h-[40px]'
+                    />
+                    <p className='text-white font-semibold md:text-sm max-w-sm xs:max-w-[90px] md:max-w-[200px] text-ellipsis whitespace-nowrap overflow-hidden '>
+                      {track.name}
+                    </p>
+                  </div>
+                  <button
+                    type='button'
+                    className='font-bold flex items-center gap-2 text-base text-green-600 px-3 py-2 cursor-pointer hover:text-green-800 mb-2 md:text-xs'
+                    onClick={() => {
+                      window.open(track.external_urls.spotify, '__blank');
+                    }}
+                  >
+                    <FaSpotify className='w-5 h-5' />
+                    {!matches && 'Open in Spotify'}
+                  </button>
+                </div>
+                {index + 1 === allTopTracks.length ? null : <hr className='border-gray-450' />}
+              </Fragment>
+            ))}
+          </>
+        )}
+      </section>
+    </main>
+  );
+}
